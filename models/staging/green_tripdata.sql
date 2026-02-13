5f372b00af6939c8ee187ab47c10ee1bb79f7f65
Nattaphat Songsi-in <eakacit@gmail.com>
2026-02-13 08:42:26 +0000

    change the green data sql file name and add yellow trip data file

--- models/staging/green_tripdata.sql
+++
@@ -1,3 +0,0 @@
-select *
---                     raw data                 table 
-from {{ source('strategic-cacao-484207-e4', 'green_tripdata')}}
---
+++ models/staging/stg_green_tripdata.sql
@@ -0,0 +1,30 @@
+select 
+    -- identifires
+    cast(vendorid as int) as vendor_id,
+    cast(ratecodeid as int) as rate_code_id,
+    cast(pulocationid as int) as pickup_location_id,
+    cast(dolocationid as int) as dropoff_location_id,
+
+    -- timestamps
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+
+    --trip info
+    store_and_fwd_flag,
+    cast(passenger_count as int) as passenger_count,
+    -- change from float to float64
+    cast(trip_distance as float64) as trip_distance,
+    cast(trip_type as int) as trip_type,
+
+    --payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(payment_type as numeric) as payment_type
+
+--                     raw data                 table 
+from {{ source('raw_data', 'green_tripdata')}}
